362ae4fd16e9cc8042a8c4e5a7778d41a1b914bb
Sujeong Ji <suji.sujeongji@gmail.com>
2020-01-31 01:09:42 +0900

    03_ionner_join

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -71,4 +71,27 @@ SELECT name, continent, code, surface_area,
 FROM countries;
  
 
+ --7. Inner Challenge : INTO
+ SELECT country_code, size,
+  CASE WHEN size > 50000000
+            THEN 'large'
+       WHEN size > 1000000
+            THEN 'medium'
+       ELSE 'small' END
+       AS popsize_group
+INTO pop_plus       
+FROM populations
+WHERE year = 2015;
+
+-- 5. Select fields
+SELECT name, continent, geosize_group, popsize_group
+-- 1. From countries_plus (alias as c)
+FROM countries_plus AS c
+  -- 2. Join to pop_plus (alias as p)
+  INNER JOIN pop_plus AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code
+-- 4. Order the table    
+ORDER BY geosize_group;
+
   
