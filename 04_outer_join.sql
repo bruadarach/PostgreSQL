1df9bbceefe3449357305039ee9334c234e3b4c3
Sujeong Ji <suji.sujeongji@gmail.com>
2020-02-01 03:21:47 +0900

    04_outer_join

---
+++ 04_outer_join.sql
@@ -0,0 +1,106 @@
+/* LEFT JOIN (1) */
+SELECT c1.name AS city, code, c2.name AS country,
+       region, city_proper_pop
+FROM cities AS c1
+  -- 1. Join right table (with alias)
+  LEFT JOIN countries AS c2
+    -- 2. Match on country code
+    ON c1.country_code = c2.code
+-- 3. Order by descending country code
+ORDER BY code DESC;
+
+
+/* LEFT JOIN (2)
+5. Select country name AS country, the country's local name,
+the language name AS language, and
+the percent of the language spoken in the country
+*/
+SELECT c.name AS country, local_name, l.name AS language, percent
+-- 1. From left table (alias as c)
+FROM countries AS c
+  -- 2. Join to right table (alias as l)
+  LEFT JOIN languages AS l
+    -- 3. Match on fields
+    ON c.code = l.code
+-- 4. Order by descending country
+ORDER BY country DESC;
+
+
+/* LEFT JOIN (3) */
+-- Select fields
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+-- From countries (alias as c)
+FROM countries AS c
+  -- Left join with economies (alias as e)
+  LEFT JOIN economies AS e
+    -- Match on code fields
+    ON c.code = e.code
+-- Focus on 2010
+WHERE year = 2010
+-- Group by region
+GROUP BY region
+-- Order by descending avg_gdp
+ORDER BY avg_gdp DESC;
+
+
+/* RIGHT JOIN */
+-- convert this code to use RIGHT JOINs instead of LEFT JOINs
+/*
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM cities
+  LEFT JOIN countries
+    ON cities.country_code = countries.code
+  LEFT JOIN languages
+    ON countries.code = languages.code
+ORDER BY city, language;
+*/
+
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM languages
+  RIGHT JOIN countries
+    ON languages.code = countries.code
+  RIGHT JOIN cities
+    ON countries.code = cities.country_code
+ORDER BY city, language;
+
+
+/* FULL JOIN (1) */
+SELECT name AS country, code, region, basic_unit
+FROM countries
+  -- 1. Join to currencies
+  FULL JOIN currencies
+    USING (code)
+-- 2. Where region is North America or null
+WHERE region = 'North America' OR region IS NULL
+-- 3. Order by region
+ORDER BY region;
+
+
+/* FULL JOIN (2) */
+SELECT countries.name, code, languages.name AS language
+FROM languages
+  -- 1. Join to countries
+  FULL JOIN countries
+    USING (code)
+-- 2. Where countries.name starts with V or is null
+WHERE countries.name LIKE 'V%' OR countries.name IS NULL
+ORDER BY countries.name;
+
+
+/* FULL JOIN (3) */
+-- 7. Select fields (with aliases)
+SELECT c1.name AS country, region, l.name AS language,basic_unit, frac_unit
+-- 1. From countries (alias as c1)
+FROM countries AS c1
+  -- 2. Join with languages (alias as l)
+  FULL JOIN languages AS l
+    -- 3. Match on code
+    USING (code)
+  -- 4. Join with currencies (alias as c2)
+  FULL JOIN currencies AS c2
+    -- 5. Match on code
+    USING (code)
+-- 6. Where region like Melanesia and Micronesia
+WHERE region LIKE 'M%esia';
