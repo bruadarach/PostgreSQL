c999f173ae01b5e38c1e57e9da38e76b5e1dc166
Sujeong Ji <suji.sujeongji@gmail.com>
2020-02-05 23:44:07 +0900

    06_subqueries

---
+++ 06_subqueries.sql
@@ -0,0 +1,8 @@
+/* Subqueries inside WHERE and SELECT clauses */
+SELECT *
+FROM populations
+WHERE life_expectancy > 1.15 *
+    (SELECT AVG(life_expectancy)
+    FROM populations
+    WHERE year = 2015)
+    AND year = 2015;
